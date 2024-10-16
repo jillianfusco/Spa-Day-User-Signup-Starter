2e2dcda1db41f8637a3148aae6dc90d7ff0b906d
Jillian Fusco <fuscojillian@gmail.com>
2024-10-16 06:59:06 -0400

    copied files after completing studio with group

---
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -0,0 +1,27 @@
+package org.launchcode.controllers;
+import org.launchcode.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("user")
+public class UserController {
+    @GetMapping("add")
+    public static String displayAddUserForm() {
+        return "/user/add";
+    }
+    @PostMapping("")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (user.getPassword().equals(verify) ) {
+            return "/user/index";
+        } else {
+            model.addAttribute("error", "passwords do not match");
+            return "/user/add";
+        }
+    }
+}
